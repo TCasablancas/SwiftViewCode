5ff10eb3edbdde8fcf332add3229a95fba866b08
Thiago Silva <t734813@santander.com.br>
2020-11-23 18:02:25 -0300

    Theme update

---
+++ SwiftViewCode/Theme/Card/CardViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewCell.swift
+//  SwiftViewCode
+//
+//  Created by Thiago Augusto da Costa Silva on 23/11/20.
+//  Copyright © 2020 Thiago Augusto da Costa Silva. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class CardViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
