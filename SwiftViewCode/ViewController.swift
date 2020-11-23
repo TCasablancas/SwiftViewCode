faaae542702dcb14f0dadf4a9e2f3519eab10fde
Thiago Silva <t734813@santander.com.br>
2020-11-23 11:29:01 -0300

    Initial Commit

---
+++ SwiftViewCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftViewCode
+//
+//  Created by Thiago Augusto da Costa Silva on 23/11/20.
+//  Copyright © 2020 Thiago Augusto da Costa Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftViewCodeTests/SwiftViewCodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftViewCodeTests.swift
+//  SwiftViewCodeTests
+//
+//  Created by Thiago Augusto da Costa Silva on 23/11/20.
+//  Copyright © 2020 Thiago Augusto da Costa Silva. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftViewCode
+
+class SwiftViewCodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
